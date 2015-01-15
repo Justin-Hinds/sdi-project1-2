2cb08d30a9fb724230a8cc29504cd99983445390
Justin-Hinds <justinhinds@gmail.com>
2015-01-15 01:17:02 -0800

    number variable attempt 1

--- Hinds_Justin_Assignment2.js
+++ Hinds_Justin_Assignment2.js
@@ -1,1 +1,30 @@
-alert("JavaScript works!");
+//alert("JavaScript works!");
+
+// Justin Hinds
+// SDI 1501
+// Project 2
+// The End of Crota
+
+// global variables
+var strClass = " Warlock "; // string
+var haveDarkBelow = true ; // bolean
+var damageOutput = 150; // number
+var crota =10000
+
+// Functions
+var getDps = function(damage){
+	var dps = damage * 5
+	for(dps < crota){
+	crota -= dps
+	console.log("Crota is down to " + crota + "hit Points!");
+	};
+	return dps;
+}; // Number Function
+
+
+
+
+//Main code
+//getDps(damageOutput)
+//console.log(getDps(damageOutput))
+getDps(damageOutput)
